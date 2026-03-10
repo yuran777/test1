75d5c45307948192a1781a10766a9d16ecd53fb2
yuan <yuan@yuanui-MacBookPro.local>
2026-03-10 10:53:09 +0900

    thumnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,48 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   viewport: "width=device-width, initial-scale=1",
+
+  title: "성우 · 유란 결혼합니다",
+  description: "2026.7.12 결혼식에 초대합니다.",
+
+  openGraph: {
+    title: "성우 · 유란 결혼합니다",
+    description: "2026.7.12 결혼식에 초대합니다.",
+    url: "https://test1-peach-six.vercel.app/invitation/sample",
+    siteName: "Weddinginvi",
+    images: [
+      {
+        url: "https://test1-peach-six.vercel.app/thumnail.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Wedding Invitation",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "성우 · 유란 결혼합니다",
+    description: "2025.11.23 웨딩홀 결혼식에 초대합니다.",
+    images: ["https://test1-peach-six.vercel.app/thumnail.jpeg"],
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
 
-        {/* 카카오 SDK */}
+        {/* Kakao SDK */}
         <Script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.8.0/kakao.min.js"
           strategy="afterInteractive"
